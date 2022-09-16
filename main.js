1499154ffdba0a82bb665e0fbafbf2134798ca2f
Emerson Alonzo Calderon <32024268+DevHubble@users.noreply.github.com>
2022-09-16 11:27:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+//defiicion de variable
+// var nom;
+// var ape;
+// var edad;
+// entrada de variables
+// nom = "John ";
+// ape = "McDonals ";
+// edad = 20;
+// salida de variables
+// alert("Hola soy " + nom + ape + "tengo " + edad + " años");
+
+//Hallar suma de 2 numeros
+// var n1;
+// var n2;
+// var suma;
+//entrada
+// n1 = parseFloat(prompt("Ingrese el primer numero: "));
+// n2 = parseFloat(prompt("Ingrese el segundo numero: "));
+//proceso
+// suma = n1 + n2;
+//salida
+// alert("la suma es: " + suma);
+
+// Calcular la potencia 2 de un número entero
+var n, c;
+// Entrada
+n = parseInt(prompt("Ingrese un numero entero"));
+// Proceso
+c = Math.pow(n, 2);
+// Salida
+alert(n + " al cuadrado es: " + c);
+
+
+// Calcular la conversion de USD a SOLES
+// var dol, sol;
+// Entrada
+// dol = parseFloat(prompt("Ingrese el monto en dolares $"));
+// Proceso
+// sol = dol * 3.89;
+// Salida
+// alert("$" + dol + " dolares en soles son S/" + sol + " soles");
+
+
+
+// Calcular el promedio de 3 notas
+// var n1, n2, n3;
+// var p;
+// alert("Ingrese 3 notas para obtener su promedio");
+// Entrada
+// n1 = parseInt(prompt("Ingrese su primera nota"));
+// n2 = parseInt(prompt("Ingrese su segunda nota"));
+// n3 = parseInt(prompt("Ingrese su tercera nota"));
+// Proceso
+// p = (n1 + n2 + n3) / 3;
+// Salida
+// if (p>=11) {
+//     alert("Excelente aprobaste el curso con: " + p);
+// } else {
+//     alert("Desaprobaste el curso con: " + p);
+// }
+
+
+
+
+// Calcular el area y perimetro de un cuadrado:
+// var nota1, nota2, nota3;
+// var promedio;
+// Entrada
+// promedio = Number(prompt("Ingrese las notas para hallar su promedio"));
+// Proceso
+// promedio = nota1 + nota2 + nota3 / 3;
+// Salida
+// alert("El promedio es: " + promedio);
+
+//tipo texto
+// const saludar = "Hola mundo"
+// document.getElementById("saludo").innerHTML = saludar;
